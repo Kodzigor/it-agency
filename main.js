80b38a6dec5b6687739c085b63f1756706574100
Igor Kodzhebash <igorkodzebash@gmail.com>
2025-06-11 18:38:01 +0300

    feat: done smooth scroll to the sections

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('.header');
+
+  window.onscroll = () => {
+    if (window.pageYOffset > 200) {
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  };
+
+  header.addEventListener('click', event => {
+    if (event.target.classList.contains('menu-link')) {
+      event.preventDefault();
+
+      const targetId = event.target.getAttribute('href').slice(1);
+      const targetElement = document.getElementById(targetId);
+      const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+      const startPosition = window.pageYOffset;
+      const distance = targetPosition - startPosition;
+
+      let startTime = null;
+
+      function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = easeInOutQuad(timeElapsed, startPosition, distance, 600);
+        window.scrollTo(0, run);
+        if (timeElapsed < 600) requestAnimationFrame(animation);
+      }
+
+      function easeInOutQuad(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+      }
+
+      requestAnimationFrame(animation);
+    }
+  });
+
   const swiper = new Swiper('.swiper', {
     slidesPerView: 2,
     spaceBetween: 20,
